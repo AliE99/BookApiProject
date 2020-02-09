472227ad8e5e8f94b47cde40faa5a86ac060930e
Ali Elahi <alielahi.1378@gmail.com>
2020-02-09 11:23:48 +0330

    Data annotation added to Models

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,12 +1,23 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookApiProject.Models
 {
     public class Author
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "first name should be less than 100 characters")]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(200, ErrorMessage = "last name should be less than 200 characters")]
         public string LastName { get; set; }
+
         public virtual Country Country { get; set; }
         public virtual ICollection<BookAuthor> BookAuthors { get; set; }
     }
