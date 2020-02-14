3eac3b39ccaa5f475924c5b03629bf8a8c123caf
Ali Elahi <alielahi.1378@gmail.com>
2020-02-14 20:48:02 +0330

    Database Seeding Class Added

--- src/Models/Review.cs
+++ src/Models/Review.cs
@@ -11,7 +11,7 @@ namespace BookApiProject.Models
 
         [Required]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "HeadLine must be between 10 and 200 characters")]
-        public string HeadLine { get; set; }
+        public string Headline { get; set; }
 
         [Required]
         [StringLength(2000, MinimumLength = 50, ErrorMessage = "ReviewText must be between 50 and 2000 chracters")]
