43138fa4c4adb2f7622788755cb3cab00dff1ad0
Ali Elahi <alielahi.1378@gmail.com>
2020-02-15 21:58:00 +0330

    Dto country class added

--- src/Controller/CountriesController.cs
+++ src/Controller/CountriesController.cs
@@ -1,25 +1,43 @@
+using System.Collections.Generic;
 using System.Linq;
+using BookApiProject.Dtos;
 using BookApiProject.Services;
 using Microsoft.AspNetCore.Mvc;
+
 namespace BookApiProject.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CountriesController: ControllerBase
+    public class CountriesController : ControllerBase
     {
-        private ICountryRepository _countryRepository;
+        private readonly ICountryRepository _countryRepository;
 
         public CountriesController(ICountryRepository countryRepository)
         {
             _countryRepository = countryRepository;
         }
-        
+
         //api/countries
         [HttpGet]
         public IActionResult GetCountries()
         {
             var countries = _countryRepository.GetCountries().ToList();
-            return Ok(countries);    
-        } 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var countriesDto = new List<CountryDto>();
+            foreach (var country in countries)
+            {
+                countriesDto.Add(new CountryDto
+                {
+                    Id = country.Id,
+                    Name = country.Name
+                });
+            }
+
+            return Ok(countriesDto);
+        }
     }
 }
